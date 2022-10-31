e54e0ade586a78e73b88583f9dc55b07d519c582
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-10-30 23:39:43 -0500

    Some code added to the proxy

--- project-1/pibl/src/main.rs
+++ project-1/pibl/src/main.rs
@@ -1,51 +1,91 @@
-use pibl::ThreadPool;
-use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
+#[macro_use]
+extern crate log;
+extern crate argparse;
+extern crate env_logger;
+extern crate mio;
+
+mod backend;
+mod sync;
+mod tcplb;
+
+use std::env;
+use std::process::exit;
+use std::sync::{Arc, Mutex};
+
+use argparse::{ArgumentParser, Collect, Store, StoreTrue};
+use mio::*;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let mut servers: Vec<String> = Vec::new();
+    let mut bind = "127.0.0.1:8000".to_string();
+    let mut redis_url = "redis://localhost".to_string();
+    let mut disable_redis = false;
+    let mut log_level = "info".to_string();
 
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Dynamic TCP load balancer");
 
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+        ap.refer(&mut servers)
+            .add_argument("server", Collect, "Servers to load balance");
+
+        ap.refer(&mut bind).add_option(
+            &["-b", "--bind"],
+            Store,
+            "Bind the load balancer to address:port (127.0.0.1:8000)",
+        );
+
+        ap.refer(&mut redis_url).add_option(
+            &["-r", "--redis"],
+            Store,
+            "URL of Redis database (redis://localhost)",
+        );
+
+        ap.refer(&mut disable_redis).add_option(
+            &["--no-redis"],
+            StoreTrue,
+            "Disable updates of backend through Redis",
+        );
+
+        ap.refer(&mut log_level).add_option(
+            &["-l", "--log"],
+            Store,
+            "Log level [debug, info, warn, error] (info)",
+        );
+
+        ap.parse_args_or_exit();
     }
 
-    println!("Shutting down.");
-}
+    env::set_var("RUST_LOG", log_level);
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    env_logger::init().unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    if servers.is_empty() {
+        println!("Need at least one server to load balance");
+        exit(1);
+    }
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
+    let backend = Arc::new(Mutex::new(
+        backend::RoundRobinBackend::new(servers).unwrap(),
+    ));
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let mut proxy = tcplb::Proxy::new(&bind, backend.clone());
+    let mut event_loop = EventLoop::new().unwrap();
 
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
+    // Register interest in notifications of new connections
+    event_loop
+        .register_opt(
+            &proxy.listen_sock,
+            Token(1),
+            EventSet::readable(),
+            PollOpt::edge(),
+        )
+        .unwrap();
 
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if !disable_redis {
+        sync::create_sync_thread(backend.clone(), redis_url);
+    }
+
+    // Start handling events
+    event_loop.run(&mut proxy).unwrap();
 }
