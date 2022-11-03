914ee19f7a834ebecfd049cead7bccb2a2f12a6f
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-11-03 14:53:43 -0500

    📄 Little typo in README.md solved

--- aws-ec2-instance-setup.sh
+++ aws-ec2-instance-setup.sh
@@ -6,5 +6,5 @@ sudo systemctl start docker
 sudo systemctl stop httpd.service
 git clone https://github.com/Youngermaster/ST0255-2022-2-Projects.git
 cd project-2/MyCompanySA
-docker build -t mycompanysa:release .
-docker run -it -p 80:8000 mycompanysa:release
+sudo docker build -t mycompanysa:release .
+sudo docker run -it -p 80:8000 mycompanysa:release
